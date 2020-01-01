f971e99695207fe9f5a265f9bde4b955cf26b94f
James Doc <hi@jamesdoc.com>
2020-01-01 18:47:12 +0000

    Add placeholder copy

--- src/views/Gift.vue
+++ src/views/Gift.vue
@@ -2,6 +2,7 @@
   <div class="centralContainer">
     <div class="gifts">
       <h1>Nothing to see here...</h1>
+      <p>We're working on this...</p>
     </div>
   </div>
 </template>
--- src/views/Haiku.vue
+++ src/views/Haiku.vue
@@ -2,6 +2,8 @@
   <div class="centralContainer">
     <div class="about">
       <h1>This is a haiku page</h1>
+      <p>Some people show love with gifts, others by words.</p>
+      <p>Read more <a href="https://en.wikipedia.org/wiki/Haiku" target="_blank" rel="noopener">about haiku</a>.</p>
     </div>
   </div>
 </template>
