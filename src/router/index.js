1f45f87fb92e9d216d8f9c81838bee44fe63901e
James Doc <hi@jamesdoc.com>
2020-01-01 18:37:13 +0000

    Add thank you page

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Rsvp from '../views/Rsvp.vue';
 import Haiku from '../views/Haiku.vue';
 import Gift from '../views/Gift.vue';
+import ThankYou from '../views/ThankYou.vue';
 
 Vue.use(VueRouter);
 
@@ -27,6 +28,11 @@ const routes = [
     path: '/gift',
     name: 'gift',
     component: Gift,
+  },
+  {
+    path: '/thankyou',
+    name: 'thankyou',
+    component: ThankYou,
   },
 ];
 
---
+++ src/views/ThankYou.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="centralContainer">
+    <div class="home">
+      <h2>Thank you for letting us know you're&nbsp;coming.</h2>
+      <p>We'll be in touch soon.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'thankyou',
+};
+</script>
