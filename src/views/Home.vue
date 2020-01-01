8627e85d7d25297d3284cccc976de61bf67b2c4e
James Doc <hi@jamesdoc.com>
2020-01-01 10:54:57 +0000

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,44 @@
 <template>
-  <div class="home">
-    James and Natalie
+  <div class="centralContainer">
+    <div class="home">
 
-    2nd May 2020
+      <h1 class="home__title">
+        James &amp; Natalie
+      </h1>
 
-    You are invited
+      <div class="home__date">
+        2nd May 2020
+      </div>
 
-    St. John's, Hoxton, 2pm
-    Pitfield Street, N1 6NP
-    Old Street (Northern Line)
+      <h2 class="home__subtitle">You're invited</h2>
 
-    <div>
-      <router-link
-        to="/rsvp"
-        class="btn btn--block"
-        aria-label="Let us know you're coming">RSVP</router-link>
+      <div class="home__location">
+        <p>
+          2pm at St. John's, Hoxton<br />
+          Pitfield Street, N1 6NP <a class="home__location__map" href="https://goo.gl/maps/gZzw11zZDM72C5dk8" target="_blank" rel="noopener">See a map</a><br />
+          <span class="home__location__underground">
+            <ug /> Old Street <span class="underground underground--northern">Northern Line</span>
+          </span>
+        </p>
+      </div>
+
+      <div>
+        <router-link
+          to="/rsvp"
+          class="btn btn--block"
+          aria-label="Let us know you're coming">RSVP</router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ug from '../assets/ug.svg';
+
 export default {
   name: 'home',
+  components: {
+    ug,
+  },
 };
 </script>
